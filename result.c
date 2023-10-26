cbfc5518482782879098e5b81ad0b14cd3cb3b4d
Love <ishemalove@gmail.com>
2023-10-26 08:09:26 -0700

    initial commit

--- result.c
+++ result.c
@@ -6,19 +6,19 @@ int main() {
     int a = 15, b = 20, c = 30;
 
     int result1 = ((x++) * (y--)) + ((++z) / 2);
-    printf("x = %d, y = %d, z = %d, result1 = %d\n", x, y, z, result1);
+    printf("x = %d, y = %d, z = %d, result1 = %d\n", x, y, z, result1);// the result is 40 because we used int and it can not put decimals
 
     x = 5;
     y = 7;
     z = 10;
     int result2 = ((++x) * (--y)) - ((z--) % 3);
-    printf("x = %d, y = %d, z = %d, result2 = %d\n", x, y, z, result2);
+    printf("x = %d, y = %d, z = %d, result2 = %d\n", x, y, z, result2);//the result is 35
 
     a = 15;
     b = 20;
     c = 30;
     int result3 = ((a-- * b) + (--c)) / 2;
-    printf("a = %d, b = %d, c = %d, result3 = %d\n", a, b, c, result3);
+    printf("a = %d, b = %d, c = %d, result3 = %d\n", a, b, c, result3);//the result is 164
 
     return 0;
 }
